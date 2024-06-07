585df752176e1dd0b36b06e6f0dea9c2fa35b905
Koalajuni <hyounjunl@gmail.com>
2024-06-07 14:32:27 +0900

    style: added home page design and login page design

--- CosChic/frontend/components/inc_footer.tsx
+++ CosChic/frontend/components/inc_footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                     <img src="./logo.png" width="80px" />
-                    <span className="ml-3 text-xl">ChicBytes</span>
+                    <span className="ml-3 text-xl">CosChic</span>
                 </a>
-                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 ChicBytes —
-                    <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@test</a>
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 ChicBytes (주)
+                    <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank"></a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500">
